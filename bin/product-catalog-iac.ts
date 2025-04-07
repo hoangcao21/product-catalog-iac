90562bc4908e434dde4bfa79e9c6a643d0e4f10f
hoangcao21 <hoangcao21@github.com>
2025-04-07 19:15:11 +0700

    chore: make stacks under root construct

--- bin/product-catalog-iac.ts
+++ bin/product-catalog-iac.ts
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { FrontendStack } from "../lib/frontend-stack";
+import { BackendStack } from "../lib/backend-stack";
 
 const app = new cdk.App();
 
-new FrontendStack(app, "ProductCatalogFrontendStack", {
+const commonConfig = {
   env: {
     account: process.env.X_AWS_ACCOUNT_ID,
     region: process.env.X_AWS_REGION,
   },
-});
+};
+
+new BackendStack(app, "ProductCatalogBackendStack", commonConfig);
+
+new FrontendStack(app, "ProductCatalogFrontendStack", commonConfig);
